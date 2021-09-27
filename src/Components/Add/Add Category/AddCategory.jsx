e6949466d9c6409bd3af015a28eed22510f92df6
Ali Hassanzadeh <Aliatraby@gmail.com>
2021-09-27 06:18:42 +0330

    Respansive Add Categorie

--- src/Components/Add/Add Category/AddCategory.jsx
+++ src/Components/Add/Add Category/AddCategory.jsx
@@ -24,14 +24,19 @@ const AddCategory = () => {
         <div className={Styles.addCategory}>
             <div className={Styles.addCategory_Main} style={{display:'flex',flexDirection:'column'}}>
                 <div className={Styles.groupColumn} style={{marginTop:'25px'}}>
-                    <div className={Styles.groupInput_title} style={{display:'flex',justifyContent:'center',alignItems:'center',marginBottom:'25px'}}>
-                        <button className={Styles.buttonCategory} onClick={submitHandler}>ثبت</button>
+                    <div className={Styles.groupInput_title}>
+                    <p className={Styles.pCategory} dir='rtl'>  نام گروه غذایی :</p>
                         <input value={value} className={Styles.inputCategory} onChange={(e)=> setValue(e.target.value)} type='text' dir="rtl" placeholder='  نام گروه غذایی ... ( از 2 کاراکتر )'/>
-                        <p className={Styles.pCategory}> : نام گروه غذایی</p>
+
                     </div> 
+
+
                 </div>
-                {value.length <2 && <p style={{color:'red', display:'flex',justifyContent:'flex-end',width:'100%',fontFamily:'iransansweb' , marginTop:"-10px"}}>نام گروه غذایی نمی تواند کمتر از 2 حرف باشد</p>
-}
+                {value.length <2 && <p className={Styles.pError}>نام گروه غذایی نمی تواند کمتر از 2 حرف باشد</p> }
+                <div className={Styles.btnParent}>
+
+                <button className={Styles.buttonCategory} onClick={submitHandler}>ثبت</button>
+                </div>
             </div>
         </div> 
     );
